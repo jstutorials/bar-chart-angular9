30d611b181e1abffc23d880760ca7016b279f9d4
Parvez Alam <Parvez.Alam@telusinternational.com>
2020-06-27 18:14:52 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label, Color } from 'ng2-charts';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pie-chart';
+  title = 'bar-chart';
+  barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  barChartLabels: Label[] = ["Pending", "InProgress", "OnHold", "Complete", "Cancelled"];
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+  barChartPlugins:any = {'backgroundColor': [
+    "#FF6384",
+ "#4BC0C0",
+ "#FFCE56",
+ "#E7E9ED",
+ "#36A2EB"
+ ]}
+ public barChartColors: Color[] = [
+  { backgroundColor: 'green' },
+]
+
+  barChartData: ChartDataSets[] = [
+    { data: [45, 37, 60, 70, 46], label: 'Task Status' }
+  ];
 }
